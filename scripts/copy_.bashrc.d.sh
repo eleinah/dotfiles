2e6384d4e6055219a1d525410e4e41bbfafaa025
helliee <ellie@lly.dev>
2025-03-09 07:59:23 -0500

    Updated SOURCE_PATH

--- scripts/copy_.bashrc.d.sh
+++ scripts/copy_.bashrc.d.sh
@@ -1,6 +1,6 @@
 # This script copies everything from ~/.bashrc.d/ to my FCOS hosts via scp
 
-SOURCE_PATH="/home/ellie/.bashrc.d/"
+SOURCE_PATH="/home/$USER/git/dotfiles/.bashrc.d/"
 DEST_PATH="~/"
 
 HOSTS=(media bunker util)

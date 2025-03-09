8185d2c16f2d1c55dd29747ffe5d716eb128106c
helliee <ellie@lly.dev>
2025-03-09 07:53:08 -0500

    I didn't like rsync

--- scripts/copy_.bashrc.d.sh
+++ scripts/copy_.bashrc.d.sh
@@ -1,10 +1,10 @@
 # This script copies everything from ~/.bashrc.d/ to my FCOS hosts via scp
 
 SOURCE_PATH="/home/ellie/.bashrc.d/"
-DEST_PATH="~/.bashrc.d/"
+DEST_PATH="~/"
 
 HOSTS=(media bunker util)
 
 for HOST in "${HOSTS[@]}"; do
-    rsync -avc "$SOURCE_PATH" "$HOST:$DEST_PATH"
+    scp -r "$SOURCE_PATH" "$HOST:$DEST_PATH"
 done

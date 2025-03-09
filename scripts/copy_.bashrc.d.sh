f3d9355ddba794fbbac44a59ffe0f0218ae00087
helliee <ellie@lly.dev>
2025-03-09 14:05:43 -0500

    Update comment

--- scripts/copy_.bashrc.d.sh
+++ scripts/copy_.bashrc.d.sh
@@ -1,4 +1,4 @@
-# This script copies everything from ~/.bashrc.d/ to my FCOS hosts via scp
+# This script copies everything from ~/git/dotfiles/.bashrc.d/ to my FCOS hosts via scp
 
 SOURCE_PATH="/home/$USER/git/dotfiles/.bashrc.d/"
 DEST_PATH="~/"

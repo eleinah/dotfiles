6a1d642e7d1d6190de0b171e9cd56c451647b8cd
helliee <ellie@lly.dev>
2025-03-24 04:19:17 -0500

    Updated fish.config and added .fish files to .config/fish/conf.d

--- .config/fish/conf.d/fastfetch.fish
+++ .config/fish/conf.d/fastfetch.fish
@@ -1,2 +1,2 @@
 # Shorthand for getting system information with fastfetch
-alias ff="fastfetch"
+alias ff 'fastfetch'
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,19 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
+# If not running interactively, don't do anything
+if not status is-interactive
+    return
 end
+
+# Aliases
+alias ls 'ls --color=auto'
+alias grep 'grep --color=auto'
+
+# User-specific environment
+if not contains $HOME/.local/bin $HOME/bin $PATH
+    set PATH $HOME/.local/bin $HOME/bin $PATH
+end
+
+# Uncomment the following line if you don't like systemctl's auto-paging feature:
+# set -x SYSTEMD_PAGE ""
 
 # Initialize Starship
 starship init fish | source

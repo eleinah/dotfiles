e034d5f7c05af31175eb8d4b249b30f3ac0dcf77
helliee <ellie@lly.dev>
2025-04-02 00:58:15 -0500

    Fixed keyword issue

--- scripts/python/github-ipset.py
+++ scripts/python/github-ipset.py
@@ -4,12 +4,12 @@ import os
 
 response = requests.get('https://api.github.com/meta')
 data = response.json()
-sets = ['hooks', 'web', 'api', 'git', 'github_enterprise_importer', 'packages', 'pages', 'importer', 'actions', 'actions_macos', 'codespaces', 'dependabot', 'copilot']
+cats = ['hooks', 'web', 'api', 'git', 'github_enterprise_importer', 'packages', 'pages', 'importer', 'actions', 'actions_macos', 'codespaces', 'dependabot', 'copilot']
 
 ips = []
 
-for set in sets:
-    for ip in data[f'{set}']:
+for cat in cats:
+    for ip in data[f'{cat}']:
         ips.append(ip)
 
 for ip in ips:

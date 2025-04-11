254f31fdc001f8546472b5a7f7209fba69ffce80
helliee <ellie@lly.dev>
2025-04-10 21:01:16 -0500

    Fixed pause timer option

--- .config/fish/conf.d/qol.fish
+++ .config/fish/conf.d/qol.fish
@@ -1,4 +1,4 @@
 alias la 'ls -a' # Shorthand to get all directories/files
 alias lla 'ls -la' # Shorthand to get all directories/files in list format
 alias wthr 'curl wttr.in' # Get the weather from wttr.in
-alias focus 'timr -m pomodoro -w 45:00 -p 10:00:00 -n on --blink on'
+alias focus 'timr -m pomodoro -w 45:00 -p 10:00 -n on --blink on'

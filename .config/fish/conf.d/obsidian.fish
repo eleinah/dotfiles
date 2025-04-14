0d7ec44e184c44b59d8f34420b8a971e434447de
helliee <ellie@lly.dev>
2025-04-14 00:44:17 -0500

    Removed obsidian.fish

--- .config/fish/conf.d/obsidian.fish
+++
@@ -1,4 +0,0 @@
-set brain /home/ellie/git/GitHub/self/obsidian-md/brain
-set braintime $(date "+%Y-%m-%d %H:%M:%S")
-
-alias brains "cd $brain; git pull --rebase; git add .; git commit -am "Updated notes $braintime"; git push"

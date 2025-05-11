03950652f566e77c0da8f3fa78a304910d4afdf2
0x311i3 <ellie@lly.dev>
2025-05-11 17:26:46 -0500

    Added zed alias

--- .config/fish/conf.d/qol.fish
+++ .config/fish/conf.d/qol.fish
@@ -2,3 +2,4 @@ alias la 'ls -a' # Shorthand to get all directories/files
 alias lla 'ls -la' # Shorthand to get all directories/files in list format
 alias wthr 'curl wttr.in' # Get the weather from wttr.in
 alias focus 'timr -m pomodoro -w 45:00 -p 10:00 -n on --blink on'
+alias zed "flatpak run dev.zed.Zed"

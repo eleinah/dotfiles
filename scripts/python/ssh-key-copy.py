a5cf950c7c4b264d1c05d5629d5ad44940ace0e3
0x311i3 <ellie@lly.dev>
2025-05-11 17:45:26 -0500

    Updated usage sections to be more concise

--- scripts/python/ssh-key-copy.py
+++ scripts/python/ssh-key-copy.py
@@ -1,12 +1,12 @@
 # This script assumes the keys you're wanting to copy are in ~/.ssh/
-# Usage: python3 ssh-key-copy.py <remote_host> <key1> [<key2> ...]
+# Usage: python3 ssh-key-copy.py <remote_host> <key1> [key2] [key3] ...
 import sys
 import subprocess
 import os
 import getpass
 
 if len(sys.argv) < 3:
-    print("Usage:\npython3 ssh-key-copy.py <remote_host> <key1> [<key2> ...]")
+    print("Usage:\npython3 ssh-key-copy.py <remote_host> <key1> [key2] [key3] ...")
     sys.exit(1)
 
 host = sys.argv[1]
@@ -18,12 +18,12 @@ for key in keys:
     if not os.path.exists(f'{ssh_dir}/{key}') or not os.path.exists(f'{ssh_dir}/{key}.pub'):
         print(f"Error: Key '{key}' or '{key}.pub' does not exist.")
         continue
-    
+
     privcmd = ['scp', '-r', f'{ssh_dir}/{key}', f'{host}:~/.ssh/']
     pubcmd = ['scp', '-r', f'{ssh_dir}/{key}.pub', f'{host}:~/.ssh/']
     privresult = subprocess.run(privcmd, capture_output=True, text=True, check=True)
     pubresult = subprocess.run(pubcmd, capture_output=True, text=True, check=True)
-    
+
     if privresult.returncode == 0:
         print(f"Successfully copied private key '{key}' to {host}")
     else:
@@ -33,4 +33,3 @@ for key in keys:
         print(f"Successfully copied public key '{key}' to {host}")
     else:
         print(f"Failed to copy public key '{key}' to {host}")
-

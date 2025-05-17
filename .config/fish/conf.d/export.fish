8049956bbc575b847dc90ff8b4b1781743ec2242
Ellie <ellie@lly.dev>
2025-05-17 04:36:24 -0500

    updated exports

--- .config/fish/conf.d/export.fish
+++ .config/fish/conf.d/export.fish
@@ -1,3 +1,4 @@
 set gh /home/ellie/workspace/github.com/0x311i3
-set brain /home/ellie/workspace/github.com/0x311i3/obsidian-md/brain
+set brain /home/ellie/workspace/github.com/0x311i3/obsidian/brain
 set PAGER bat
+set ws /home/ellie/workspace

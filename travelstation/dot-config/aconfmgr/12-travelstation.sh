05b7d30aefb428b9ce08ed44b9149773403de717
Ellie <github@lly.email>
2025-07-28 09:47:49 -0500

    fix: add iwd dir for travelstation

--- travelstation/dot-config/aconfmgr/12-travelstation.sh
+++ travelstation/dot-config/aconfmgr/12-travelstation.sh
@@ -1,6 +1,8 @@
-AddPackage intel-media-driver # for hardware accelerated decoding
-AddPackage --foreign evsieve # lenovo pen support
-AddPackage iio-sensor-proxy # screen rotation detection and tablet mode
+AddPackage intel-media-driver    # for hardware accelerated decoding
+AddPackage --foreign evsieve     # lenovo pen support
+AddPackage iio-sensor-proxy      # screen rotation detection and tablet mode
 AddPackage power-profiles-daemon # laptop power saving modes
-AddPackage dhcpcd
-AddPackage iwd
+AddPackage dhcpcd                # for DHCP resolution
+AddPackage iwd                   # for Wi-Fi capabilities
+
+CreateDir /etc/iwd # for Wi-Fi capabilities

bd5c0873a8472f3bb2b8a713c211d76d2797099d
Ellie <github@lly.email>
2025-08-02 18:33:18 -0500

    feat: add vpn pkg for work

--- travelstation/dot-config/aconfmgr/12-travelstation.sh
+++ travelstation/dot-config/aconfmgr/12-travelstation.sh
@@ -1,8 +1,9 @@
-AddPackage intel-media-driver    # for hardware accelerated decoding
-AddPackage --foreign evsieve     # lenovo pen support
-AddPackage iio-sensor-proxy      # screen rotation detection and tablet mode
-AddPackage power-profiles-daemon # laptop power saving modes
-AddPackage dhcpcd                # for DHCP resolution
-AddPackage iwd                   # for Wi-Fi capabilities
+AddPackage intel-media-driver            # for hardware accelerated decoding
+AddPackage --foreign evsieve             # lenovo pen support
+AddPackage iio-sensor-proxy              # screen rotation detection and tablet mode
+AddPackage power-profiles-daemon         # laptop power saving modes
+AddPackage dhcpcd                        # for DHCP resolution
+AddPackage iwd                           # for Wi-Fi capabilities
+AddPackage globalprotect-openconnect-git # A GUI for GlobalProtect VPN, based on OpenConnect, supports the SSO authentication method.
 
 CreateDir /etc/iwd # for Wi-Fi capabilities

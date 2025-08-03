326b285a93da2c72ce4a60a15891065cbdb88f34
Ellie <github@lly.email>
2025-08-03 00:16:36 -0500

    feat: add vmware vmrc for work

--- travelstation/dot-config/aconfmgr/12-travelstation.sh
+++ travelstation/dot-config/aconfmgr/12-travelstation.sh
@@ -5,5 +5,6 @@ AddPackage power-profiles-daemon                   # laptop power saving modes
 AddPackage dhcpcd                                  # for DHCP resolution
 AddPackage iwd                                     # for Wi-Fi capabilities
 AddPackage --foreign globalprotect-openconnect-git # A GUI for GlobalProtect VPN, based on OpenConnect, supports the SSO authentication method.
+AddPackage --foreign vmware-vmrc                   # Remote Console for VMware (used for work, vSphere)
 
 CreateDir /etc/iwd # for Wi-Fi capabilities

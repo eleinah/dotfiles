6ac150758b8a99243c513bbf61c34c79fd903d88
Ellie <github@lly.email>
2025-08-10 21:32:07 -0500

    dev: remove out of date package

--- common/dot-config/aconfmgr/80-foreign.sh
+++ common/dot-config/aconfmgr/80-foreign.sh
@@ -1,21 +1,20 @@
 # Foreign (AUR) packages
-AddPackage --foreign aconfmgr-git              # A configuration manager for Arch Linux
-AddPackage --foreign flameshot-git             # Powerful yet simple to use screenshot software
-AddPackage --foreign jetbrains-toolbox         # Manage all your JetBrains Projects and Tools
-AddPackage --foreign license                   # A tool to easily add a license to your project
-AddPackage --foreign otf-departure-mono        # a monospaced pixel font with a lo-fi technical vibe
-AddPackage --foreign slack-desktop-wayland     # Slack Desktop (Beta) for Linux with Wayland Support
-AddPackage --foreign spicetify-cli             # Command-line tool to customize Spotify client
-AddPackage --foreign spicetify-marketplace-bin # Spicetify app that integrates a marketplace for extensions, themes and snippets
-AddPackage --foreign spotify                   # A proprietary music streaming service
-AddPackage --foreign swaylock-effects          # A fancier screen locker for Wayland.
-AddPackage --foreign vesktop                   # A standalone Electron-based Discord app with Vencord & improved Linux support
-AddPackage --foreign waybar-cava               # Highly customizable Wayland bar for Sway and Wlroots based compositors, with module cava (Cross-platform Audio Visualizer)
-AddPackage --foreign yay                       # Yet another yogurt. Pacman wrapper and AUR helper written in go.
-AddPackage --foreign yay-debug                 # Detached debugging symbols for yay
-AddPackage --foreign gomodifytags              # Go tool to modify struct field tags
-AddPackage --foreign gore                      # Yet another Go REPL that works nicely
-AddPackage --foreign gotests                   # Golang commandline tool that generates table driven tests based on its target source files function and method signatures
-AddPackage --foreign scc                       # Sloc, Cloc and Code: a very fast accurate code counter with complexity calculations and COCOMO estimates written in pure Go
-AddPackage --foreign powershell-bin # A cross-platform automation and configuration tool/framework (binary package)
+AddPackage --foreign aconfmgr-git               # A configuration manager for Arch Linux
+AddPackage --foreign flameshot-git              # Powerful yet simple to use screenshot software
+AddPackage --foreign jetbrains-toolbox          # Manage all your JetBrains Projects and Tools
+AddPackage --foreign license                    # A tool to easily add a license to your project
+AddPackage --foreign otf-departure-mono         # a monospaced pixel font with a lo-fi technical vibe
+AddPackage --foreign spicetify-cli              # Command-line tool to customize Spotify client
+AddPackage --foreign spicetify-marketplace-bin  # Spicetify app that integrates a marketplace for extensions, themes and snippets
+AddPackage --foreign spotify                    # A proprietary music streaming service
+AddPackage --foreign swaylock-effects           # A fancier screen locker for Wayland.
+AddPackage --foreign vesktop                    # A standalone Electron-based Discord app with Vencord & improved Linux support
+AddPackage --foreign waybar-cava                # Highly customizable Wayland bar for Sway and Wlroots based compositors, with module cava (Cross-platform Audio Visualizer)
+AddPackage --foreign yay                        # Yet another yogurt. Pacman wrapper and AUR helper written in go.
+AddPackage --foreign yay-debug                  # Detached debugging symbols for yay
+AddPackage --foreign gomodifytags               # Go tool to modify struct field tags
+AddPackage --foreign gore                       # Yet another Go REPL that works nicely
+AddPackage --foreign gotests                    # Golang commandline tool that generates table driven tests based on its target source files function and method signatures
+AddPackage --foreign scc                        # Sloc, Cloc and Code: a very fast accurate code counter with complexity calculations and COCOMO estimates written in pure Go
+AddPackage --foreign powershell-bin             # A cross-platform automation and configuration tool/framework (binary package)
 AddPackage --foreign powershell-editor-services # A common platform for PowerShell development support in any editor or application

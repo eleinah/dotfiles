06182f6ddee35234b59e2333db909d64407dec1b
Ellie <github@lly.email>
2025-08-12 01:02:52 -0500

    fix: remove google chrome

--- travelstation/dot-config/aconfmgr/12-travelstation.sh
+++ travelstation/dot-config/aconfmgr/12-travelstation.sh
@@ -5,6 +5,5 @@ AddPackage power-profiles-daemon                   # laptop power saving modes
 AddPackage dhcpcd                                  # for DHCP resolution
 AddPackage iwd                                     # for Wi-Fi capabilities
 AddPackage --foreign globalprotect-openconnect-git # A GUI for GlobalProtect VPN, based on OpenConnect, supports the SSO authentication method.
-AddPackage --foreign google-chrome                 # The popular browser by Google
 
 CreateDir /etc/iwd # for Wi-Fi capabilities

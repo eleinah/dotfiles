4a8c5952ff07f4c628ce0044c89f88e7450d10a7
Eleina Hayden <eleina@member.fsf.org>
2025-08-13 14:10:21 -0500

    feat: add bolt-launcher

--- common/dot-config/aconfmgr/80-foreign.sh
+++ common/dot-config/aconfmgr/80-foreign.sh
@@ -19,3 +19,4 @@ AddPackage --foreign scc                        # Sloc, Cloc and Code: a very fa
 AddPackage --foreign powershell-bin             # A cross-platform automation and configuration tool/framework (binary package)
 AddPackage --foreign powershell-editor-services # A common platform for PowerShell development support in any editor or application
 AddPackage --foreign runelite                   # Open source Old School RuneScape client.
+AddPackage --foreign bolt-launcher              # Free open-source third-party implementation of the Jagex Launcher

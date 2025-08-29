e4ffd89a27dbb21ae89a88b90ba07c019fd74ee9
Eleina Hayden <eleina@member.fsf.org>
2025-08-29 10:06:55 -0500

    feat: add bluetooth capabilities

--- travelstation/dot-config/aconfmgr/12-travelstation.sh
+++ travelstation/dot-config/aconfmgr/12-travelstation.sh
@@ -5,5 +5,8 @@ AddPackage power-profiles-daemon                   # laptop power saving modes
 AddPackage dhcpcd                                  # for DHCP resolution
 AddPackage iwd                                     # for Wi-Fi capabilities
 AddPackage --foreign globalprotect-openconnect-git # A GUI for GlobalProtect VPN, based on OpenConnect, supports the SSO authentication method.
+AddPackage bluetui # TUI for managing bluetooth devices
+AddPackage bluez # Daemons for the bluetooth protocol stack
+AddPackage bluez-utils # Development and debugging utilities for the bluetooth protocol stack
 
 CreateDir /etc/iwd # for Wi-Fi capabilities
